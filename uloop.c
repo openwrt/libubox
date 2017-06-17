openwrt/libubox

commit 368fd2645878edadc72c60948d1f19c6769751d6

uloop: allow specifying a timeout for uloop_run()
This can be useful for cleanup with pending timers, or for hooking into
existing code that does not use uloop

Signed-off-by: Felix Fietkau <nbd@nbd.name>

uloop.c
@@ -520,8 +520,9 @@ bool uloop_cancelling(void)
 	return uloop_run_depth > 0 && uloop_cancelled;
 }
 
-int uloop_run(void)
+int uloop_run_timeout(int timeout)
 {
+	int next_time = 0;
 	struct timeval tv;
 
 	/*
@@ -545,7 +546,11 @@ int uloop_run(void)
 			break;
 
 		uloop_gettime(&tv);
-		uloop_run_events(uloop_get_next_timeout(&tv));
+
+		next_time = uloop_get_next_timeout(&tv);
+		if (timeout > 0 && next_time < timeout)
+			timeout = next_time;
+		uloop_run_events(timeout);
 	}
 
 	if (!--uloop_run_depth)

uloop.h
@@ -105,7 +105,11 @@ static inline void uloop_end(void)
 }
 
 int uloop_init(void);
-int uloop_run(void);
+int uloop_run_timeout(int timeout);
+static inline int uloop_run(void)
+{
+	return uloop_run_timeout(-1);
+}
 void uloop_done(void);
 
 #endif

commit 4bc3decf8743c0add93d434a639bb1b449b6da11

uloop: fix a regression in timeout handling
Variable confusion was breaking timers

Fixes: 368fd2645878 ("uloop: allow specifying a timeout for uloop_run()")
Signed-off-by: Felix Fietkau <nbd@nbd.name>

uloop.c
@@ -548,9 +548,9 @@ int uloop_run_timeout(int timeout)
 		uloop_gettime(&tv);
 
 		next_time = uloop_get_next_timeout(&tv);
-		if (timeout > 0 && next_time < timeout)
-			timeout = next_time;
-		uloop_run_events(timeout);
+		if (timeout > 0 && timeout < next_time)
+			next_time = timeout;
+		uloop_run_events(next_time);
 	}
 
 	if (!--uloop_run_depth)