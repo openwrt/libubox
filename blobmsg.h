openwrt/libubox

commit 9e52171d70def760a6949676800d0b73f85ee22d

blobmsg: introduce BLOBMSG_CAST_INT64
When dealing with 64-bit integers in JSON documents, blobmsg_parse
becomes useless as blobmsg-json only uses BLOBMSG_TYPE_INT64 if the
value exceeds the range of a 32-bit integer, otherwise
BLOBMSG_TYPE_INT32 is used. This is because blobmsg-json parses the
JSON document ad-hoc without knowing the schema in advance and hence
a result of the design of blobmsg-json (and the absence of JSON
schema definitions).
In practise, this made code less readable as instead of using
blobmsg_parse() one had to to deal with *all* attributes manually just
to catch fields which can be both, BLOBMSG_TYPE_INT32 or
BLOBMSG_TYPE_INT64, but are always dealt with as uint64_t in code as
they potentially could exceed the 32-bit range.

To resolve this issue, introduce as special wildcard attribute
type BLOBMSG_CAST_INT64 which should only be used in policies used
by blobmsg_parse(). If used for an attribute in the policy,
blobmsg_parse shall accept all integer types and allow the user
to retrieve the value using the uint64_t blobmsg_cast_u64() and
int64_t blobmsg_cast_s64() functions which is also introduced by this
commit.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

blobmsg.h
@@ -35,6 +35,7 @@ enum blobmsg_type {
 	__BLOBMSG_TYPE_LAST,
 	BLOBMSG_TYPE_LAST = __BLOBMSG_TYPE_LAST - 1,
 	BLOBMSG_TYPE_BOOL = BLOBMSG_TYPE_INT8,
+	BLOBMSG_CAST_INT64,
 };
 
 struct blobmsg_hdr {
@@ -285,6 +286,38 @@ static inline uint64_t blobmsg_get_u64(struct blob_attr *attr)
 	uint32_t *ptr = (uint32_t *) blobmsg_data(attr);
 	uint64_t tmp = ((uint64_t) be32_to_cpu(ptr[0])) << 32;
 	tmp |= be32_to_cpu(ptr[1]);
+	return tmp;
+}
+
+static inline uint64_t blobmsg_cast_u64(struct blob_attr *attr)
+{
+	uint64_t tmp = 0;
+
+	if (blobmsg_type(attr) == BLOBMSG_TYPE_INT64)
+		tmp = blobmsg_get_u64(attr);
+	else if (blobmsg_type(attr) == BLOBMSG_TYPE_INT32)
+		tmp = blobmsg_get_u32(attr);
+	else if (blobmsg_type(attr) == BLOBMSG_TYPE_INT16)
+		tmp = blobmsg_get_u16(attr);
+	else if (blobmsg_type(attr) == BLOBMSG_TYPE_INT8)
+		tmp = blobmsg_get_u8(attr);
+
+	return tmp;
+}
+
+static inline int64_t blobmsg_cast_s64(struct blob_attr *attr)
+{
+	int64_t tmp = 0;
+
+	if (blobmsg_type(attr) == BLOBMSG_TYPE_INT64)
+		tmp = blobmsg_get_u64(attr);
+	else if (blobmsg_type(attr) == BLOBMSG_TYPE_INT32)
+		tmp = (int32_t)blobmsg_get_u32(attr);
+	else if (blobmsg_type(attr) == BLOBMSG_TYPE_INT16)
+		tmp = (int16_t)blobmsg_get_u16(attr);
+	else if (blobmsg_type(attr) == BLOBMSG_TYPE_INT8)
+		tmp = (int8_t)blobmsg_get_u8(attr);
+
 	return tmp;
 }
 

blobmsg.c
@@ -195,7 +195,15 @@ int blobmsg_parse(const struct blobmsg_policy *policy, int policy_len,
 				continue;
 
 			if (policy[i].type != BLOBMSG_TYPE_UNSPEC &&
+			    policy[i].type != BLOBMSG_CAST_INT64 &&
 			    blob_id(attr) != policy[i].type)
+				continue;
+
+			if (policy[i].type == BLOBMSG_CAST_INT64 &&
+			    (blob_id(attr) != BLOBMSG_TYPE_INT64 &&
+			     blob_id(attr) != BLOBMSG_TYPE_INT32 &&
+			     blob_id(attr) != BLOBMSG_TYPE_INT16 &&
+			     blob_id(attr) != BLOBMSG_TYPE_INT8))
 				continue;
 
 			if (blobmsg_namelen(hdr) != pslen[i])

commit 2e52c7e9a90ab7ba1cf9d2986d1505ca5d184698

libubox: fix BLOBMSG_CAST_INT64 (do not override BLOBMSG_TYPE_DOUBLE)
Commit 9e52171 ('blobmsg: introduce BLOBMSG_CAST_INT64') broke
blobmsg_parse() for BLOBMSG_TYPE_DOUBLE.

This is because the enum definition leads to the following double
define for BLOBMSG_CAST_INT64/BLOBMSG_TYPE_DOUBLE as value 8.

Tested with:

	$ cat test-enum-001.c
  #include <stdio.h>

  enum blobmsg_type {
  	BLOBMSG_TYPE_UNSPEC,
  	BLOBMSG_TYPE_ARRAY,
  	BLOBMSG_TYPE_TABLE,
  	BLOBMSG_TYPE_STRING,
  	BLOBMSG_TYPE_INT64,
  	BLOBMSG_TYPE_INT32,
  	BLOBMSG_TYPE_INT16,
  	BLOBMSG_TYPE_INT8,
  	BLOBMSG_TYPE_DOUBLE,
  	__BLOBMSG_TYPE_LAST,
  	BLOBMSG_TYPE_LAST = __BLOBMSG_TYPE_LAST - 1,
  	BLOBMSG_TYPE_BOOL = BLOBMSG_TYPE_INT8,
  	BLOBMSG_CAST_INT64,
  };

  int main(int artc, char* argv[]) {
  	printf("BLOBMSG_TYPE_UNSPEC: %d\n", BLOBMSG_TYPE_UNSPEC);
  	printf("BLOBMSG_TYPE_ARRAY: %d\n", BLOBMSG_TYPE_ARRAY);
  	printf("BLOBMSG_TYPE_TABLE: %d\n", BLOBMSG_TYPE_TABLE);
  	printf("BLOBMSG_TYPE_STRING: %d\n", BLOBMSG_TYPE_STRING);
  	printf("BLOBMSG_TYPE_INT64: %d\n", BLOBMSG_TYPE_INT64);
  	printf("BLOBMSG_TYPE_INT32: %d\n", BLOBMSG_TYPE_INT32);
  	printf("BLOBMSG_TYPE_INT16: %d\n", BLOBMSG_TYPE_INT16);
  	printf("BLOBMSG_TYPE_INT8: %d\n", BLOBMSG_TYPE_INT8);
  	printf("BLOBMSG_TYPE_DOUBLE: %d\n", BLOBMSG_TYPE_DOUBLE);
  	printf("__BLOBMSG_TYPE_LAST: %d\n", __BLOBMSG_TYPE_LAST);
  	printf("BLOBMSG_TYPE_LAST: %d\n", BLOBMSG_TYPE_LAST);
  	printf("BLOBMSG_TYPE_BOOL: %d\n", BLOBMSG_TYPE_BOOL);
  	printf("BLOBMSG_CAST_INT64: %d\n", BLOBMSG_CAST_INT64);
  	return 0;
  }

	$ gcc test-enum-001.c

	$ ./a.out
  BLOBMSG_TYPE_UNSPEC: 0
  BLOBMSG_TYPE_ARRAY: 1
  BLOBMSG_TYPE_TABLE: 2
  BLOBMSG_TYPE_STRING: 3
  BLOBMSG_TYPE_INT64: 4
  BLOBMSG_TYPE_INT32: 5
  BLOBMSG_TYPE_INT16: 6
  BLOBMSG_TYPE_INT8: 7
  BLOBMSG_TYPE_DOUBLE: 8
  __BLOBMSG_TYPE_LAST: 9
  BLOBMSG_TYPE_LAST: 8
  BLOBMSG_TYPE_BOOL: 7
  BLOBMSG_CAST_INT64: 8

Fix this by changing the enum defintion to assign BLOBMSG_CAST_INT64 to

blobmsg.h
@@ -31,11 +31,11 @@ enum blobmsg_type {
 	BLOBMSG_TYPE_INT32,
 	BLOBMSG_TYPE_INT16,
 	BLOBMSG_TYPE_INT8,
+	BLOBMSG_TYPE_BOOL = BLOBMSG_TYPE_INT8,
 	BLOBMSG_TYPE_DOUBLE,
 	__BLOBMSG_TYPE_LAST,
 	BLOBMSG_TYPE_LAST = __BLOBMSG_TYPE_LAST - 1,
-	BLOBMSG_TYPE_BOOL = BLOBMSG_TYPE_INT8,
-	BLOBMSG_CAST_INT64,
+	BLOBMSG_CAST_INT64 = __BLOBMSG_TYPE_LAST,
 };
 
 struct blobmsg_hdr {