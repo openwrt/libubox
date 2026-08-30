openwrt/libubox

commit 6fc29d1c429240a9afb71c22aa161b066502f3ed

jshn.sh: Add pretty-printing to json_dump
If a JSON file might be read by a human, say for debugging, it
could be useful to pretty-print it.  We do this in places by
calling "json_dump -i" but it shouldn't be necessary to know the
arguments to "jshn" (and indeed, that's not portable if we retool
the underlying implementation). Conversely output that's ephemeral
doesn't need to be pretty (say being piped as input to another
command).

Signed-off-by: Philip Prindeville <philipp@redfish-solutions.com>

sh/jshn.sh
@@ -199,6 +199,16 @@ json_add_fields() {
 
 # functions read access to json variables
 
+json_compact() {
+	JSON_NONEWLINE=1
+	JSON_INDENT=
+}
+
+json_pretty() {
+	JSON_NONEWLINE=
+	JSON_INDENT=1
+}
+
 json_load() {
 	eval "`jshn -r "$1"`"
 }
@@ -208,7 +218,7 @@ json_load_file() {
 }
 
 json_dump() {
-	jshn "$@" ${JSON_PREFIX:+-p "$JSON_PREFIX"} -w 
+	jshn "$@" ${JSON_PREFIX:+-p "$JSON_PREFIX"} ${JSON_NONEWLINE:+-n} ${JSON_INDENT:+-i} -w
 }
 
 json_get_type() {

commit 34a2dd08a21c921733a44615753e745ed3202bf0

jshn.sh: add missing unset of JSON variables to cleanup
The behavior of output should also be cleared on json_cleanup(),
which should be expected to remove all evidence of JSON usage
from the shell environment for a certain JSON context with JSON_PREFIX
except for the definition of JSON_PREFIX itself.

Fixes: 6fc29d1c4 ("jshn.sh: Add pretty-printing to json_dump")
Signed-off-by: Michael Pratt <mcpratt@pm.me>

sh/jshn.sh
@@ -115,6 +115,8 @@ json_cleanup() {
 	done
 
 	unset \
+		JSON_NONEWLINE \
+		JSON_INDENT \
 		${JSON_PREFIX}JSON_SEQ \
 		${JSON_PREFIX}JSON_CUR \
 		${JSON_PREFIX}JSON_UNSET