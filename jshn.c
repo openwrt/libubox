openwrt/libubox

commit 26586dae43a83d286762c73b49b98aa8dfc852c3

jshn: fix missing usage for -p and -o arguments
Add missing usage hints for -p and -o arguments.

Fixes: e16fa068a573 ("jshn: add support for namespaces")
Fixes: eb30a03048f8 ("libubox, jshn: add option to write output to a file")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

jshn.c
@@ -311,7 +311,7 @@ out:
 
 static int usage(const char *progname)
 {
-	fprintf(stderr, "Usage: %s [-n] [-i] -r <message>|-R <file>|-w\n", progname);
+	fprintf(stderr, "Usage: %s [-n] [-i] -r <message>|-R <file>|-o <file>|-p <prefix>|-w\n", progname);
 	return 2;
 }
 

tests/cram/test_jshn.t
@@ -6,7 +6,7 @@ set jshn for convenience:
 check usage:
 
   $ jshn
-  Usage: jshn [-n] [-i] -r <message>|-R <file>|-w
+  Usage: jshn [-n] [-i] -r <message>|-R <file>|-o <file>|-p <prefix>|-w
   [2]
 
 test bad json:

commit 15cbc762772868deb8464b914829bcf7c522d4f5

jshn: fix usage syntax helper text
In the usage text output, which is printed
when incorrect syntax or no parameters are given,
it appears that <prefix> is shown as a mode of execution,
when it is actually an optional parameter that affects all modes.

Also, the position suggests that it can be after a mode,
or by itself as a mode, when that would be incorrect syntax.

Let the prefix option be shown earlier and in brackets as optional.

Fixes: 26586dae4 ("jshn: fix missing usage for -p and -o arguments")
Signed-off-by: Michael Pratt <mcpratt@pm.me>

jshn.c
@@ -308,7 +308,7 @@ out:
 
 static int usage(const char *progname)
 {
-	fprintf(stderr, "Usage: %s [-n] [-i] -r <message>|-R <file>|-o <file>|-p <prefix>|-w\n", progname);
+	fprintf(stderr, "Usage: %s [-n] [-i] [-p prefix] -r <message>|-R <file>|-o <file>|-w\n", progname);
 	return 2;
 }
 

tests/cram/test_jshn.t
@@ -6,11 +6,11 @@ set jshn for convenience:
 check usage:
 
   $ jshn
-  Usage: jshn [-n] [-i] -r <message>|-R <file>|-o <file>|-p <prefix>|-w
+  Usage: jshn [-n] [-i] [-p prefix] -r <message>|-R <file>|-o <file>|-w
   [2]
 
   $ jshn-san
-  Usage: jshn-san [-n] [-i] -r <message>|-R <file>|-o <file>|-p <prefix>|-w
+  Usage: jshn-san [-n] [-i] [-p prefix] -r <message>|-R <file>|-o <file>|-w
   [2]
 
 test bad json: